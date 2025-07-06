68d7ece371600e9c35664ee56e87333bc04311b6
MC <corman.r.2017@gmail.com>
2025-07-06 15:27:27 +0300

    Fixed a warning-throwing line of code & made the arrow black.

--- scripts/classes/components/state machines/state listeners/Human Player/AmuletThrowingStateListener.gd
+++ scripts/classes/components/state machines/state listeners/Human Player/AmuletThrowingStateListener.gd
@@ -10,5 +10,5 @@ func _on_state_entered(): ## Shows where the arrow is pointing.
 func _on_state_exited(): ## Makes the arrow invisible.
 	object.visible = false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	object.rotation = (global_position - state.mouse_pos).angle()
