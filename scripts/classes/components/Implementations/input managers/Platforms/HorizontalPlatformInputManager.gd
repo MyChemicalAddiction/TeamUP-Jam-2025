2ae5b4e801e8e0fdbb57c3c6fb3807671ca7a1eb
MC <corman.r.2017@gmail.com>
2025-07-15 21:14:39 +0300

    Added vertical cursed platforms.

--- scripts/classes/components/Implementations/input managers/Platforms/HorizontalPlatformInputManager.gd
+++ scripts/classes/components/Implementations/input managers/Platforms/HorizontalPlatformInputManager.gd
@@ -1,5 +1,5 @@
 extends InputManager
-class_name HorizontalPlatformInputManager
+class_name GhostPlatformInputManager
 
 """
 Encapsulates input logic for horizontal possessable platforms.
@@ -8,5 +8,9 @@ Encapsulates input logic for horizontal possessable platforms.
 var horizontal_input := 0.0
 var vertical_input := 0.0
 
+@export var horizontal := false
+@export var vertical := false
+
 func process_input():
-	horizontal_input = Input.get_axis("ghost_move_left", "ghost_move_right")
+	if horizontal: horizontal_input = Input.get_axis("ghost_move_left", "ghost_move_right")
+	if vertical: vertical_input = Input.get_axis("ghost_move_up", "ghost_move_down")
