96c4484450a7fec8ec1cfc96e506b517c4a375bb
MC <corman.r.2017@gmail.com>
2025-07-18 20:54:49 +0300

    Re-added accidentally deleted scene reset functionality that was removed in last commit.

--- scripts/singletons/SceneManager.gd
+++ scripts/singletons/SceneManager.gd
@@ -5,6 +5,9 @@ The scene manager singleton delegates scene switching functionality to a main
 scene.
 """
 
+## Holds the current scene so it can be reset.
+var current_scene: PackedScene
+
 ## Caught by all nodes that need to know when the scene is changing
 signal loading_scene
  
@@ -12,5 +15,10 @@ signal loading_scene
 signal change_scene(new_scene: PackedScene)
 
 func transition_to_scene(new_scene: PackedScene): # For transitioning the player's location to a different scene
-	change_scene.emit(new_scene)
+	current_scene = new_scene
+	
+	change_scene.emit(current_scene)
 	loading_scene.emit()
+
+func reset_scene():
+	change_scene.emit(current_scene)
