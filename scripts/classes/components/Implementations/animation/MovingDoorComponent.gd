7bf68da840e702b1e4136021e552fdb298a308f3
MC <corman.r.2017@gmail.com>
2025-07-18 22:47:15 +0300

    Added horizontal moving doors (that can be triggered). Also added a prototype level!

--- scripts/classes/components/Implementations/animation/MovingDoorComponent.gd
+++ scripts/classes/components/Implementations/animation/MovingDoorComponent.gd
@@ -7,20 +7,34 @@ button is being pressed).
 """
 
 @export var object: Node ## The door that needs to move up/down
-@export var MOVING_DISTANCE := 100 ## How far should the door move up/down
+@export var MOVING_DISTANCE := 450 ## How far should the door move up/down
 @export var MOVING_TIME := 0.6 ## THe time in seconds for the object to open/close
 
 @onready var current_tween : Tween ## The current tween being used
 
 @onready var closed_pos = object.position
-@onready var open_pos = closed_pos - Vector2(0, MOVING_DISTANCE)
+
+var open_pos: Vector2
+
+@export_enum("Vertical", "Horizontal") var orientation: int = 0
+
+func _ready():
+	super()
+	if !orientation: open_pos = closed_pos - Vector2(0, MOVING_DISTANCE)
+	else: open_pos = closed_pos - Vector2(MOVING_DISTANCE, 0)
 
 func _on_enable():
 	if current_tween: current_tween.kill()
 	current_tween = self.create_tween()
-	current_tween.tween_property(object, "position", open_pos, MOVING_TIME * (abs(object.position.y - open_pos.y) / MOVING_DISTANCE))
-
+	if orientation:
+		current_tween.tween_property(object, "position", open_pos, MOVING_TIME * (abs(object.position.x - open_pos.x) / MOVING_DISTANCE))
+	else:
+		current_tween.tween_property(object, "position", open_pos, MOVING_TIME * (abs(object.position.y - open_pos.y) / MOVING_DISTANCE))
+		
 func _on_disable():
 	if current_tween: current_tween.kill()
 	current_tween = self.create_tween()
-	current_tween.tween_property(object, "position", closed_pos, MOVING_TIME * (abs(object.position.y - closed_pos.y) / MOVING_DISTANCE))
+	if orientation:
+		current_tween.tween_property(object, "position", closed_pos, MOVING_TIME * (abs(object.position.x - closed_pos.x) / MOVING_DISTANCE))
+	else:
+		current_tween.tween_property(object, "position", closed_pos, MOVING_TIME * (abs(object.position.y - closed_pos.y) / MOVING_DISTANCE))
--- scripts/classes/components/Implementations/input managers/Characters/Ghost/GhostInteractInputManager.gd
+++ scripts/classes/components/Implementations/input managers/Characters/Ghost/GhostInteractInputManager.gd
@@ -8,4 +8,4 @@ Manages input for the ghost's HumanInteractInputManager.
 var pressed_interact := false
 
 func process_input():
-	pressed_interact = Input.is_action_just_pressed("ghost_interact")
+	pressed_interact = Input.is_action_just_released("ghost_interact")
