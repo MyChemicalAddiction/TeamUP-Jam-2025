b3749f219f430a2996563bcb4783ceb498fdf424
MC <corman.r.2017@gmail.com>
2025-07-21 13:45:02 +0300

    Tweaked level 1, added ambience to all levels, reduced amulet bounce.

--- scripts/classes/functionality/PlayersStartMarker.gd
+++ scripts/classes/functionality/PlayersStartMarker.gd
@@ -10,3 +10,6 @@ func _ready():
 	var players = get_tree().get_nodes_in_group("Player")
 	for i in players:
 		i.global_position = global_position
+	
+	var camera = get_tree().get_first_node_in_group("camera")
+	camera.global_position = global_position
