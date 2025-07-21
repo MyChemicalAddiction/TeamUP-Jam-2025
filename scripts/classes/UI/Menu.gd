3c3a3b83c0a24315fc2d36f4d67831d10644bcc6
MC <corman.r.2017@gmail.com>
2025-07-21 18:01:37 +0300

    Modified amulet.

--- scripts/classes/UI/Menu.gd
+++ scripts/classes/UI/Menu.gd
@@ -18,8 +18,15 @@ Menu.
 ## The current visible UI section
 @export var current_section: Control
 
+var default_section: Control
+
 ## The node reference to the main game scene (once instanced)
 var main_game_scene: Node
+
+func _ready():
+	default_section = current_section
+	current_section.visible = true
+	set_process(false) ## Makes it so Escape doesn't do anything when already in menu.
 
 ## Called to switch to another UI section (E.g. from any section to Settings. Settings section should be pased as the new_section)
 func switch_section(new_section):
@@ -31,8 +38,26 @@ func switch_section(new_section):
 ## Loads a level as passed by a LevelButton's signal
 func instantiate_game(level: PackedScene):
 	current_section.visible = false ## Hides the current UI section
+	set_process(true)
+	
+	if main_game_scene:
+		main_game_scene.process_mode = Node.PROCESS_MODE_INHERIT
+		SceneManager.transition_to_scene(level)
+		return
 	
 	main_game_scene = main_game_scene_res.instantiate() 
+	
 	main_game_scene.default_scene = level ## Sets the main scene to load the level passed 
 	
 	add_child(main_game_scene) ## Adds the new scene
+
+## Allows opening the menu during play
+func _process(_delta) -> void:
+	if main_game_scene:
+		if Input.is_action_just_pressed("toggle_menu"):
+			if !current_section.visible:
+				main_game_scene.process_mode = Node.PROCESS_MODE_DISABLED
+				switch_section(default_section)
+			else:
+				current_section.visible = false
+				main_game_scene.process_mode = Node.PROCESS_MODE_INHERIT
