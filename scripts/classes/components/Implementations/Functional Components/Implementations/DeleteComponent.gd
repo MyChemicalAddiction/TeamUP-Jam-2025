2b6136e66e7800896653c37207d238df3e2b7faa
MC <corman.r.2017@gmail.com>
2025-07-25 20:45:38 +0300

    Added tutorial pointers to level 3 (which will probably become level two in the final build.)

--- scripts/classes/components/Implementations/Functional Components/Implementations/DeleteComponent.gd
+++ scripts/classes/components/Implementations/Functional Components/Implementations/DeleteComponent.gd
@@ -6,11 +6,12 @@ You assign the object you need deleted to the export object variable.
 extends FunctionalComponent
 class_name DeleteComponent
 
-@export var object: Node ## The object that is to be deleted.
+@export var objects: Array[Node] ## The objects that are to be deleted.
 
 func _on_use(): ## TODO: remove the if usable from here; make usable an inherited attribute; wrap this function in a use() function in parent class
 	_delete()
 
 func _delete(): ## Deletes the object.
-	object.queue_free()
+	for object in objects:
+		object.queue_free()
 	queue_free()
