16754946d6a149bad7038131db02d1d68ceb617b
MC <corman.r.2017@gmail.com>
2025-07-26 19:47:33 +0300

    Added SFX on possessing & entering/exiting amulet.

---
+++ scripts/functionality/audio/FMODPlayingStateListener.gd
@@ -0,0 +1,14 @@
+extends StateListener
+
+@export var fmod_event_emitter: FmodEventEmitter2D
+
+@export var on_enter := true
+@export var on_exit := false
+
+func _on_state_entered(): ## Overridden to provide behavior on the state being entered.
+	if on_enter:
+		fmod_event_emitter.play()
+
+func _on_state_exited():
+	if on_exit:
+		fmod_event_emitter.play()
