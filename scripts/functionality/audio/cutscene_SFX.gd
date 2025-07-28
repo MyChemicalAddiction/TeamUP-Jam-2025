80b340b6c854bd63540769dcd72dab1752ba7bef
MC <corman.r.2017@gmail.com>
2025-07-28 21:20:52 +0300

    Added SFX to cutscenes.

---
+++ scripts/functionality/audio/cutscene_SFX.gd
@@ -0,0 +1,13 @@
+extends AudioStreamPlayer
+
+@onready var cutscene_player = get_parent()
+
+func _ready() -> void:
+	cutscene_player.visibility_changed.connect(_toggle)
+
+func _toggle():
+	if cutscene_player.visible:
+		play()
+	else:
+
+		stop()
