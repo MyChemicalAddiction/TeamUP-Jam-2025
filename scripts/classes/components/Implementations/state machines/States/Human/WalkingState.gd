4ba9c02b1718ca74f002a7d7c7f4eab5aab88769
MC <corman.r.2017@gmail.com>
2025-07-30 22:33:19 +0300

    Added coyote time.

--- scripts/classes/components/Implementations/state machines/States/Human/WalkingState.gd
+++ scripts/classes/components/Implementations/state machines/States/Human/WalkingState.gd
@@ -8,12 +8,22 @@ class_name HumanWalkingState
 @export var SPEED = 300.0
 @export var ACCELERATE = 60.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 var vertical_input := 0.0
 var horizontal_input := 0.0
 
 @export var push_component: PushComponent
 
 @export var visual: AnimatedSprite2D
+
+## Used for coyote time
+@export var coyote_timer : Timer
+var falling
+
+func _ready():
+	super()
+	coyote_timer.timeout.connect(fall)
 
 func process_input(_delta):
 	input_manager.process_input()
@@ -28,9 +38,24 @@ func process_input(_delta):
 	if vertical_input < 0:
 		return JumpingState
 
+func fall():
+	falling = true
+
 func process_physics(_delta: float):
-	if not object.is_on_floor():
+	if falling:
+		falling = false
 		return FallingState
+		
+	if not object.is_on_floor():
+		if coyote_timer.is_stopped():
+			coyote_timer.start()
+		else:
+			object.velocity.y += gravity * _delta
+			
+	else:
+		coyote_timer.timeout.disconnect(fall)
+		coyote_timer.stop()
+		coyote_timer.timeout.connect(fall)
 	
 	object.move_and_slide()
 	
