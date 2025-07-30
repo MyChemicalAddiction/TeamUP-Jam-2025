a7d6e7a231b70f5890d53466c4a47e0f8995f434
MC <corman.r.2017@gmail.com>
2025-07-30 13:44:23 +0300

    Made ghost not clip into walls.

--- scripts/functionality/stuck_detector.gd
+++ scripts/functionality/stuck_detector.gd
@@ -18,6 +18,8 @@ until it doesn't detect any more bodies inside it. Then, it re-activates it.
 
 @export var enabled := true
 
+@onready var object = get_tree().get_first_node_in_group("Ghost")
+
 var area_present := false
 
 func _ready():
@@ -27,6 +29,8 @@ func _ready():
 	ghost_hidden_state.entered.connect(disable)
 	ghost_hidden_state.exited.connect(enable)
 	
+	ghost_hidden_state.exited.connect(check)
+	
 	get_tree().get_first_node_in_group("amulet_radius").on_enabled.connect(_on_amulet_radius_enabled)
 	get_tree().get_first_node_in_group("amulet_radius").on_disabled.connect(_on_amulet_radius_disabled)
 	
@@ -35,7 +39,16 @@ func _ready():
 
 func _on_amulet_radius_disabled():
 	if area_present:
-		physical_collision.set_deferred('disabled', true)
+		var max_attempts := 8
+		var motion := Vector2(1, 0).rotated(randf() * TAU)  # random initial direction
+		var distance := 4.0  # push distance per attempt
+
+		for i in range(max_attempts):
+			var test_motion := motion.rotated(i * TAU / max_attempts) * distance
+			var result := PhysicsTestMotionResult2D.new()
+			if not object.test_move(object.transform, test_motion, result):
+				object.global_position += test_motion
+				break
 
 func _on_amulet_radius_enabled():
 	pass
@@ -43,7 +56,6 @@ func _on_amulet_radius_enabled():
 func _on_area_empty():
 	if enabled:
 		area_present = false
-		physical_collision.set_deferred('disabled', false)
 
 func _on_area_present():
 	area_present = true
@@ -53,3 +65,6 @@ func disable():
 
 func enable():
 	enabled = true
+
+func check():
+	pass
