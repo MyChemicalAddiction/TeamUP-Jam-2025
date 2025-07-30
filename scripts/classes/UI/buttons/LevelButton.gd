ecb90836d526a25e0595b6b1f15463da7c0560b9
MC <corman.r.2017@gmail.com>
2025-07-30 15:48:02 +0300

    Added level remembering logic & lighting to level 5.

--- scripts/classes/UI/buttons/LevelButton.gd
+++ scripts/classes/UI/buttons/LevelButton.gd
@@ -32,7 +32,9 @@ func _ready():
 	SaveLoad.loaded.connect(update)
 
 func update():
-	if level_path in SaveLoad.get_data("level_completed_data"):
+	if level_path in SaveLoad.get_data("level_completed_data") and level_path:
+		print(level_path)
+		
 		if !("(Completed)" in text): text += " (Completed)"
 	else:
 		text = original_text
