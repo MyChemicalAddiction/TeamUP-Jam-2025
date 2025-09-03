de7a11020fe38aaa71d39c42960217618c8b3016
MC <corman.r.2017@gmail.com>
2025-09-03 22:32:04 +0300

    Added SFX to fullscreen button.

--- scenes/UI/fullscreenbutton.gd
+++ scenes/UI/fullscreenbutton.gd
@@ -1,11 +1,13 @@
 extends Button
 
+@export var audio_player: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button_up.connect(toggle_fullscreen)
 	
 func toggle_fullscreen():
+	audio_player.play()
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 	else:
