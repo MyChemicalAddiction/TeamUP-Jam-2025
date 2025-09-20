2eb7001508a61fa2ba3a6b4e543a2afc22082209
MC <corman.r.2017@gmail.com>
2025-09-20 21:23:55 +0300

    Working on amulet audio.

---
+++ scripts/functionality/audio/integrated-audio/land_audio.gd
@@ -0,0 +1,15 @@
+extends SFXRandomizer
+
+"""
+Plays when body_entered is emitted by object.
+"""
+
+@export var object : Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	object.body_entered.connect(_play)
+
+func _play(_body):
+	if !get_tree().paused:
+		play(true)
