d0a36391e6e7f97b67c834b1db5fe7cd2d88d252
MC <corman.r.2017@gmail.com>
2025-09-20 20:58:58 +0300

    Added integrated SFX to portals.

--- scripts/classes/functionality/audio/integrated-audio/SFXRandomizer/implementations/SFXRandomizer.gd
+++ scripts/classes/functionality/audio/integrated-audio/SFXRandomizer/implementations/SFXRandomizer.gd
@@ -6,8 +6,14 @@ Holds multiple audiostream resources from which it randomly selects one to make
 a delegated AudioStreamPlayer play.
 '''
 
+@export var autoplay := false ## If this plays when the scene is loaded
+
 @export var audio_player: AudioStreamPlayer
 @export var audio_list: Array[AudioStream]
+
+func _ready():
+	if autoplay:
+		play()
 
 func play(param=null):
 	audio_player.stream = audio_list.pick_random()
